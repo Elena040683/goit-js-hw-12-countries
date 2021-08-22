36551f17ed06ac4a5e777f21bd81ba6d09e2aedc
Elena <Elena3405674@mail.ru>
2021-08-22 19:58:39 +0300

    commit3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import debounce from 'lodash.debounce';
 import { alert, error } from '../node_modules/@pnotify/core/dist/PNotify.js';
 import '@pnotify/core/dist/BrightTheme.css';
+import fetchCountries from './js/fetchCountries';
+import countryTpl from './templates/country.hbs';
 
 const refs = {
   input: document.querySelector('#search'),
@@ -12,39 +14,36 @@ refs.input.addEventListener('input', debounce(onInputChange, 500));
 function onInputChange(e) {
   e.preventDefault();
   const searchQuery = refs.input.value;
-  fetchCountries(searchQuery);
+  fetchCountries(searchQuery).then(data => countriesLoadResult(data));
 }
 
-function fetchCountries(searchQuery) {
-  fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
-    .then(response => response.json())
-    .then(data => {
-      onMoreThenTenCountriesPerPage(data);
-      renderCollection(data.name);
-    })
-    .catch(err => console.log(err));
-}
-
-function onMoreThenTenCountriesPerPage(data) {
-  if (data.length > 10) {
+function countriesLoadResult(data) {
+  if (data.length === 1) {
+    resetSearch();
+    countryRender(data);
+  } else if (data.length > 1 && data.length <= 10) {
+    resetSearch();
+    renderCollection(data);
+  } else if (data.length > 10) {
     error({
       text: 'Too many matches found. Please enter a more specific query!',
     });
-    return;
   }
 }
 
-function onTwoUpTenCountriesPerPage(data) {
-  if (data.length < 10 && data.length > 2) {
-    const country = `<ul><li>${data.name}</li>
-    </ul>`;
+function resetSearch() {
+  refs.container.innerHTML = '';
+}
 
-    refs.container.insertAdjacentHTML('beforeend', country);
-  }
+function markupCountries({ name }) {
+  const country = `<li>${name}</li>`;
+  refs.container.insertAdjacentHTML('beforeend', country);
 }
 
 function renderCollection(arr) {
-  arr.forEach(el => {
-    onTwoUpTenCountriesPerPage(el);
-  });
+  arr.forEach(el => markupCountries(el));
+}
+
+function countryRender() {
+  refs.container.insertAdjacentHTML('beforeend', countryTpl);
 }
